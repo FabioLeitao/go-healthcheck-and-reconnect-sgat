e533fc4b902c584d9f5a71d97196e3e73f283186
FabioLeitao <fabio.tleitao@gmail.com>
2024-01-23 16:05:53 -0300

    Update default.go
    Atualizacao de segurança

--- pkg/oracle/default.go
+++ pkg/oracle/default.go
@@ -5,7 +5,7 @@ import (
 )
 
 func NewConnection() *sql.DB {
-	conn, err := sql.Open("oracle", "oracle://tosp:Athenas2018@10.129.48.68/tosprd")
+	conn, err := sql.Open("oracle", "oracle://tosp:A****8@10.129.48.68/tosprd")
 	if err != nil {
 		panic(err)
 	}
